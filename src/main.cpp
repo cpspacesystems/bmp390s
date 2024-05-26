2c536c081f9020a78df3f5a64ce51b06dc13089c
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-05-26 10:20:58 -0700

    change to single-BMP I2C demo code

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,48 @@
-#include <Arduino.h>
+/***************************************************************************
+  This is a library for the BMP3XX temperature & pressure sensor
+
+  Designed specifically to work with the Adafruit BMP388 Breakout
+  ----> http://www.adafruit.com/products/3966
+
+  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+  to interface.
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing products
+  from Adafruit!
+
+  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
+#include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 
-#define BMP_SCK 13
-#define BMP_MISO 12
-#define BMP_MOSI 11
-#define BMP_CS 10
-#define BMP2_CS 0
+// #define BMP_SCK 19
+// #define BMP_MISO 12
+// #define BMP_MOSI 11
+// #define BMP_CS 10
+
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BMP3XX bmp;
-Adafruit_BMP3XX bmp2;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  while(!Serial); // Burn clock cycles until serial starts
-  Serial.println("Test started");
-  if(!bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {
-    Serial.println("Could not find a valid BMP3 sensor.");
-    while(1); // Burn clock cycles if failure. Program should be terminated.
-  }
+  while (!Serial)
+    ;
+  Serial.println("Adafruit BMP388 / BMP390 test");
 
-  if(!bmp2.begin_SPI(BMP2_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {
-    Serial.println("Could not find a valid second BMP3 sensor.");
-    while(1); // Burn clock cycles if failure. Program should be terminated.
+  if (!bmp.begin_I2C(0x76, &Wire))
+  { // hardware I2C mode, can pass in address & alt Wire
+    // if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
+    // if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1)
+      ;
   }
 
   // Set up oversampling and filter initialization
@@ -32,36 +50,25 @@ void setup() {
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
-
-  bmp2.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp2.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp2.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp2.setOutputDataRate(BMP3_ODR_50_HZ);
 }
 
-void loop() {
-  if (! bmp.performReading()) {
+void loop()
+{
+  if (!bmp.performReading())
+  {
     Serial.println("Failed to perform reading :(");
     return;
   }
   Serial.print("Temperature = ");
   Serial.print(bmp.temperature);
   Serial.println(" *C");
-  Serial.print(bmp2.temperature);
-  Serial.println(" *C");
 
   Serial.print("Pressure = ");
   Serial.print(bmp.pressure / 100.0);
   Serial.println(" hPa");
-  Serial.print(bmp2.pressure / 100.0);
-  Serial.println(" hPa");
-
-  Serial.print("Approx. Altitude = ");
   Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
-  Serial.print(bmp2.readAltitude(SEALEVELPRESSURE_HPA));
   Serial.println(" m");
 
   Serial.println();
-  delay(500);
+  delay(2000);
 }
