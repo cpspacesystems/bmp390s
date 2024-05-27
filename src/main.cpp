ad1c365368d6d011820c3669b21075d7a645d147
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-05-27 06:46:24 -0700

    wire via spi

--- src/main.cpp
+++ src/main.cpp
@@ -20,10 +20,11 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 
-// #define BMP_SCK 19
-// #define BMP_MISO 12
-// #define BMP_MOSI 11
-// #define BMP_CS 10
+#define BMP_SCK 13
+#define BMP_MISO 12
+#define BMP_MOSI 11
+#define BMP_CS 10
+#define BMP2_CS 37
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
@@ -37,64 +38,63 @@ void setup()
     ;
   Serial.println("Adafruit BMP388 / BMP390 test");
 
-  if (!bmp.begin_I2C(0x76, &Wire))
-  { // hardware I2C mode, can pass in address & alt Wire
-    // if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
-    // if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+  // Set up BMP1 via SPI
+  if (!bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI))
+  { 
     Serial.println("Could not find a valid FIRST BMP3 sensor, check wiring!");
     while (1)
       ;
   }
-  if (!bmp2.begin_I2C(0x77, &Wire))
-  { // hardware I2C mode, can pass in address & alt Wire
-    // if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode
-    // if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
-    Serial.println("Could not find a valid SECOND BMP3 sensor, check wiring!");
-    while (1)
-      ;
+  
+  // Set up BMP2 via SPI (the only thing that changes is the CS pin)
+  if (!bmp2.begin_SPI(BMP2_CS, BMP_SCK, BMP_MISO, BMP_MOSI))
+  {   
+      Serial.println("Could not find a valid SECOND BMP3 sensor, check wiring!");
+      while (1)
+        ;
   }
 
-  // Set up oversampling and filter initialization
-  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+    // Set up oversampling and filter initialization
+    bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
-  bmp2.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
-  bmp2.setPressureOversampling(BMP3_OVERSAMPLING_4X);
-  bmp2.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
-  bmp2.setOutputDataRate(BMP3_ODR_50_HZ);
-}
+    bmp2.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+    bmp2.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+    bmp2.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+    bmp2.setOutputDataRate(BMP3_ODR_50_HZ);
+  }
 
-void loop()
-{
-  if (!bmp.performReading())
+  void loop()
   {
-    Serial.println("Failed to perform reading :(");
-    return;
+    if (!bmp.performReading())
+    {
+      Serial.println("Failed to perform reading :(");
+      return;
+    }
+    Serial.print("Temperature 1 = ");
+    Serial.print(bmp.temperature);
+    Serial.println(" *C");
+
+    Serial.print("Temperature 2 = ");
+    Serial.print(bmp2.temperature);
+    Serial.println(" *C");
+
+    Serial.print("Pressure 1 = ");
+    Serial.print(bmp.pressure / 100.0);
+    Serial.println(" hPa");
+
+    Serial.print("Pressure 2 = ");
+    Serial.print(bmp2.pressure / 100.0);
+    Serial.println(" hPa");
+
+    Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.print(bmp2.readAltitude(SEALEVELPRESSURE_HPA));
+    Serial.println(" m");
+
+    Serial.println();
+    delay(2000);
   }
-  Serial.print("Temperature 1 = ");
-  Serial.print(bmp.temperature);
-  Serial.println(" *C");
-
-  Serial.print("Temperature 2 = ");
-  Serial.print(bmp2.temperature);
-  Serial.println(" *C");
-
-  Serial.print("Pressure 1 = ");
-  Serial.print(bmp.pressure / 100.0);
-  Serial.println(" hPa");
-
-  Serial.print("Pressure 2 = ");
-  Serial.print(bmp2.pressure / 100.0);
-  Serial.println(" hPa");
-
-  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
-
-  Serial.print(bmp2.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
-
-  Serial.println();
-  delay(2000);
-}
